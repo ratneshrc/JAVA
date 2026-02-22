6850853e59628fbab65b7a9ece5c0e5355999add
ratneshrc <ratneshchavan349@gmail.com>
2026-02-22 14:41:14 +0530

    Merge.java

--- Merge.java
+++ Merge.java
@@ -2,25 +2,20 @@ public class Merge {
     public static void main(String[] args) {
 
         int arr1[] = {1,2,3,4,5};
-        int arr2[] = {6,7,8,9, 10};
-
-        // create new array
+        int arr2[] = {6,7,8,9,10};
         int arr3[] = new int[arr1.length + arr2.length];
-
-        // copy arr1 into arr3
         for (int i = 0; i < arr1.length; i++) {
             arr3[i] = arr1[i];
         }
-
-        // copy arr2 into arr3
-        for (int i = 0; i < arr2.length; i++) {
+        for (int i = 0; i < arr2.length; i++)
+        {
             arr3[arr1.length + i] = arr2[i];
         }
-
-        // print merged array
         System.out.println("Merged Array:");
-        for (int i = 0; i < arr3.length; i++) {
+        for (int i = 0; i < arr3.length; i++)
+        {
             System.out.print(arr3[i] + " ");
         }
     }
 }
+
