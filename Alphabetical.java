864e40ed8067e688d9ec76964a4094665207b7db
ratneshrc <ratneshchavan349@gmail.com>
2026-02-22 14:53:36 +0530

    Create Alphabetical.java

---
+++ Alphabetical.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Sorting{
+public static void main(String[] args){
+String[] names={"Jonthan","Clutchgod","Zgod","Neyoo","Admino","Spowerr","Manyaa","Godz","Wmax","Simpp"};
+
+Arrays.sort(names);
+for(String name:names){
+System.out.println(name);
+}
+}
+}
