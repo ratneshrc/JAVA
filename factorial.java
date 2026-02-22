41fe9cda98955403f34eaa587617a070e602fa04
ratneshrc <ratneshchavan349@gmail.com>
2026-02-22 14:25:28 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,10 +1,11 @@
 public class Fact {
     public static void main(String[] arg){
-        int a=10;
+        int a=7;
         int fact=1;
         for(int i=a;i>=1;i--){
             fact=fact*i;
         }
-        System.out.println("Factorial of 10 is"+fact);
+        System.out.println("Factorial of 7 is " + fact);
     }
+
 }
