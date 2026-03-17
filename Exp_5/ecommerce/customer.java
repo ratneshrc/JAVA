25351c21e02debd8661fde88709de6cbf7e51775
ratneshrc <ratneshchavan349@gmail.com>
2026-03-17 09:52:10 +0530

    Create customer.java

---
+++ Exp_5/ecommerce/customer.java
@@ -0,0 +1,13 @@
+package Ecommerce;
+
+public class Customer {
+    String name;
+
+    public Customer(String n) {
+        name = n;
+    }
+
+    public void displayCustomer() {
+        System.out.println(name);
+    }
+}
